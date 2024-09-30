c155377f7c5d2572bec7ff69a416aedb9f61ac15
ddan25 <peterortiz97@yahoo.com>
2024-09-29 21:38:20 -0700

    implemented abletotow on truck class

--- Develop/src/classes/Cli.ts
+++ Develop/src/classes/Cli.ts
@@ -277,7 +277,7 @@ class Cli {
 
   // method to find a vehicle to tow
   // TODO: add a parameter to accept a truck object
-  findVehicleToTow(): void {
+  findVehicleToTow(truck: Truck): void {
     inquirer
       .prompt([
         {
@@ -294,8 +294,15 @@ class Cli {
       ])
       .then((answers) => {
         // TODO: check if the selected vehicle is the truck
+        if (answers.vehicleToTow.vin === truck.vin) {
         // TODO: if it is, log that the truck cannot tow itself then perform actions on the truck to allow the user to select another action
+        console.log('The truck cannot tow itself');
+        this.performActions(truck);
         // TODO: if it is not, tow the selected vehicle then perform actions on the truck to allow the user to select another action
+        } else {
+          truck.tow(answers.vehicleToTow);
+          this.performActions(truck);
+        }
       });
   }
 
