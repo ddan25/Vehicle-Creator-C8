244c2a84d5ec33460578dc4b4b9259611b4be5b3
ddan25 <peterortiz97@yahoo.com>
2024-09-29 20:40:20 -0700

    add statements to create a truck or motorbike if the user selects the respective vehicle type

--- Develop/src/classes/Cli.ts
+++ Develop/src/classes/Cli.ts
@@ -61,15 +61,19 @@ class Cli {
           name: 'vehicleType',
           message: 'Select a vehicle type',
           // TODO: Update the choices array to include Truck and Motorbike
-          choices: ['Car'],
+          choices: ['Car', 'Truck', 'Motorbike'],
         },
       ])
       .then((answers) => {
         if (answers.vehicleType === 'Car') {
           // create a car
           this.createCar();
+             // TODO: add statements to create a truck or motorbike if the user selects the respective vehicle type
+        } else if (answers.vehicleType === 'Truck') {
+          this.createTruck();
+        } else if (answers.vehicleType === 'Motorbike') {
+          this.createMotorbike();
         }
-        // TODO: add statements to create a truck or motorbike if the user selects the respective vehicle type
       });
   }
 
